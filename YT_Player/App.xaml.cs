128d993147eb35175d9b2ddb1828bc2dacfcfefc
Robin <156453367+hbgroca@users.noreply.github.com>
2025-02-25 18:11:25 +0100

    Main commit

---
+++ YT_Player/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace YT_Player;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
---
+++ YT_Player/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Input;
+using Microsoft.Web.WebView2.Core;
+
+namespace YT_Player;
+
+public partial class MainWindow : Window
+{
+    private string userDataFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "DBP_Player", "DBP_Player_YT");
+    private bool isResizing = false;
+    private Point startPoint;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        InitializeAsync();
+    }
+
+    // Initialize WebView2
+    private async void InitializeAsync()
+    {
+        var options = new CoreWebView2EnvironmentOptions();
+        var environment = await CoreWebView2Environment.CreateAsync(null, userDataFolder, options);
+
+        await webView.EnsureCoreWebView2Async(environment);
+        webView.CoreWebView2.Navigate("https://music.youtube.com/");
+    }
+
+
+    // Drag and drop window
+    private void TopBar_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ChangedButton == MouseButton.Left)
+        {
+            DragMove();
+        }
+    }
+
+
+    // Resize window
+    private void ResizeGrip_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ChangedButton == MouseButton.Left)
+        {
+            isResizing = true;
+            startPoint = e.GetPosition(this);
+            Mouse.Capture((UIElement)sender);
+        }
+    }
+
+    private void ResizeGrip_MouseMove(object sender, MouseEventArgs e)
+    {
+        if (isResizing)
+        {
+            Point currentPoint = e.GetPosition(this);
+            double deltaX = currentPoint.X - startPoint.X;
+            double deltaY = currentPoint.Y - startPoint.Y;
+
+            Width = Math.Max(300, Width + deltaX);
+            Height = Math.Max(425, Height + deltaY); 
+
+            startPoint = currentPoint;
+        }
+    }
+
+    private void ResizeGrip_MouseUp(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ChangedButton == MouseButton.Left)
+        {
+            isResizing = false;
+            Mouse.Capture(null);
+        }
+    }
+
+
+    // Close window
+    private void ExitButton_Click(object sender, RoutedEventArgs e)
+    {
+        Environment.Exit(0);
+    }
+}
